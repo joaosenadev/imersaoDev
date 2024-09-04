b2ff2839406a72ff4f6ab5d6bdc74f52dcaad75c
João Sena <joaocarlosbdesena@gmail.com>
2024-09-04 19:21:33 -0300

    Aula 03 - Adicionando dados ao arquivos JS

---
+++ dados.js
@@ -0,0 +1,73 @@
+let dados = [
+    {
+        titulo: "Post Malone",
+        descricao: `Post Malone é um dos artistas mais influentes da atualidade, conhecido por sua voz rouca característica e pela mescla de diversos gêneros musicais em suas canções. Com hits como "Sunflower" e "Rockstar", ele conquistou milhões de fãs ao redor do mundo.`,
+        redeSocial: "https://www.instagram.com/postmalone/",
+        musicaPopular: "https://www.youtube.com/watch?v=ApXoWvfEYVU",
+        wiki: "https://pt.wikipedia.org/wiki/Post_Malone",
+    },
+    {
+        titulo: "Taylor Swift",
+        descricao: `Taylor Swift é uma das maiores artistas da música pop, conhecida por suas letras emocionantes e melodias cativantes. Com álbuns como "1989" e "Folklore", ela quebrou recordes e conquistou fãs de todas as idades.`,
+        redeSocial: "https://www.instagram.com/taylorswift/",
+        musicaPopular: "https://www.youtube.com/watch?v=nfWlot6h_JM",
+        wiki: "https://pt.wikipedia.org/wiki/Taylor_Swift",
+    },
+    {
+        titulo: "The Weeknd",
+        descricao: `The Weeknd é um cantor e compositor canadense conhecido por sua voz suave e melodias R&B. Com hits como "Blinding Lights" e "Starboy", ele se tornou um dos artistas mais populares da música contemporânea.`,
+        redeSocial: "https://www.instagram.com/theweeknd/",
+        musicaPopular: "https://www.youtube.com/watch?v=4NRXx6U8ABQ",
+        wiki: "https://pt.wikipedia.org/wiki/The_Weeknd",
+    },
+    {
+        titulo: "Ariana Grande",
+        descricao: "Cantora pop americana conhecida por sua voz poderosa e performances energéticas.",
+        redeSocial: "https://www.instagram.com/arianagrande/",
+        musicaPopular: "https://www.youtube.com/watch?v=L9mE-V9n34k", // thank u, next
+        wiki: "https://pt.wikipedia.org/wiki/Ariana_Grande"
+    },
+    {
+        titulo: "BTS",
+        descricao: "Grupo sul-coreano de K-pop que conquistou o mundo com suas músicas e coreografias sincronizadas.",
+        redeSocial: "https://www.instagram.com/bts.bighitofficial/",
+        musicaPopular: "https://www.youtube.com/watch?v=kX07pm6I_Qs", // Dynamite
+        wiki: "https://pt.wikipedia.org/wiki/BTS"
+    },
+    {
+        titulo: "Dua Lipa",
+        descricao: "Cantora britânica de música pop conhecida por seus hits dançantes e estilo único.",
+        redeSocial: "https://www.instagram.com/dualipa/",
+        musicaPopular: "https://www.youtube.com/watch?v=pXRViK1CV8A", // Levitating
+        wiki: "https://pt.wikipedia.org/wiki/Dua_Lipa"
+    },
+    {
+        titulo: "Ed Sheeran",
+        descricao: "Cantor e compositor britânico conhecido por suas canções românticas e voz suave.",
+        redeSocial: "https://www.instagram.com/teddysphotos/",
+        musicaPopular: "https://www.youtube.com/watch?v=LGK8tod5G94", // Shape of You
+        wiki: "https://pt.wikipedia.org/wiki/Ed_Sheeran"
+    },
+    {
+        titulo: "Lady Gaga",
+        descricao: "Cantora e compositora americana conhecida por sua excentricidade e versatilidade musical.",
+        redeSocial: "https://www.instagram.com/ladygaga/",
+        musicaPopular: "https://www.youtube.com/watch?v=qrO4YZeyl0I", // Shallow
+        wiki: "https://pt.wikipedia.org/wiki/Lady_Gaga"
+    },
+    {
+        titulo: "Rihanna",
+        descricao: "Cantora barbadiana conhecida por seus hits dançantes e estilo fashion.",
+        redeSocial: "https://www.instagram.com/badgalriri/",
+        musicaPopular: "https://www.youtube.com/watch?v-QGJuZ8jw_8", // Umbrella
+        wiki: "https://pt.wikipedia.org/wiki/Rihanna"
+    },
+    {
+        titulo: "Justin Bieber",
+        descricao: "Cantor canadense que se tornou famoso ainda na adolescência.",
+        redeSocial: "https://www.instagram.com/justinbieber/",
+        musicaPopular: "https://www.youtube.com/watch?v=kX07pm6I_Qs", // Sorry
+        wiki: "https://pt.wikipedia.org/wiki/Justin_Bieber"
+    }
+]
+
