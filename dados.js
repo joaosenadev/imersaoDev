8249e04f57ed7fa2411b7c6c9b7980d6ecfb3428
João Sena <joaocarlosbdesena@gmail.com>
2024-09-05 19:40:15 -0300

    Aula 04 - Incluindo dados no HTML c/ JS

--- dados.js
+++ dados.js
@@ -1,73 +1,72 @@
 let dados = [
     {
         titulo: "Post Malone",
-        descricao: `Post Malone é um dos artistas mais influentes da atualidade, conhecido por sua voz rouca característica e pela mescla de diversos gêneros musicais em suas canções. Com hits como "Sunflower" e "Rockstar", ele conquistou milhões de fãs ao redor do mundo.`,
+        descricao: `Post Malone é um dos artistas mais inovadores da música contemporânea, conhecido por sua voz rouca e pela mistura única de rap, rock e pop. Com sucessos globais como "Sunflower" e "Rockstar", ele continua a cativar uma vasta audiência com seu estilo eclético.`,
         redeSocial: "https://www.instagram.com/postmalone/",
         musicaPopular: "https://www.youtube.com/watch?v=ApXoWvfEYVU",
         wiki: "https://pt.wikipedia.org/wiki/Post_Malone",
     },
     {
         titulo: "Taylor Swift",
-        descricao: `Taylor Swift é uma das maiores artistas da música pop, conhecida por suas letras emocionantes e melodias cativantes. Com álbuns como "1989" e "Folklore", ela quebrou recordes e conquistou fãs de todas as idades.`,
+        descricao: `Taylor Swift é uma das maiores estrelas da música pop, reconhecida por suas letras introspectivas e melodias cativantes. Com álbuns icônicos como "1989" e "Folklore", ela não só quebrou recordes como também conquistou uma legião de fãs ao redor do mundo.`,
         redeSocial: "https://www.instagram.com/taylorswift/",
         musicaPopular: "https://www.youtube.com/watch?v=nfWlot6h_JM",
         wiki: "https://pt.wikipedia.org/wiki/Taylor_Swift",
     },
     {
         titulo: "The Weeknd",
-        descricao: `The Weeknd é um cantor e compositor canadense conhecido por sua voz suave e melodias R&B. Com hits como "Blinding Lights" e "Starboy", ele se tornou um dos artistas mais populares da música contemporânea.`,
+        descricao: `The Weeknd é um artista canadense aclamado por sua voz distintiva e suas canções imersivas no gênero R&B. Com sucessos como "Blinding Lights" e "Starboy", ele solidificou sua posição como um dos ícones da música moderna.`,
         redeSocial: "https://www.instagram.com/theweeknd/",
         musicaPopular: "https://www.youtube.com/watch?v=4NRXx6U8ABQ",
         wiki: "https://pt.wikipedia.org/wiki/The_Weeknd",
     },
     {
         titulo: "Ariana Grande",
-        descricao: "Cantora pop americana conhecida por sua voz poderosa e performances energéticas.",
+        descricao: `Ariana Grande é uma estrela pop americana conhecida por sua impressionante gama vocal e performances energéticas. Com hits como "Thank U, Next", ela conquistou uma vasta base de fãs e continua a definir tendências no mundo da música.`,
         redeSocial: "https://www.instagram.com/arianagrande/",
-        musicaPopular: "https://www.youtube.com/watch?v=gl1aHhXnN1k", // thank u, next
+        musicaPopular: "https://www.youtube.com/watch?v=gl1aHhXnN1k",
         wiki: "https://pt.wikipedia.org/wiki/Ariana_Grande"
     },
     {
         titulo: "BTS",
-        descricao: "Grupo sul-coreano de K-pop que conquistou o mundo com suas músicas e coreografias sincronizadas.",
+        descricao: `BTS é um fenômeno global do K-pop, conhecido por suas performances eletrizantes e coreografias precisas. Com músicas como "Dynamite", o grupo sul-coreano não só revolucionou o cenário musical, mas também conquistou uma legião de fãs ao redor do mundo.`,
         redeSocial: "https://www.instagram.com/bts.bighitofficial/",
-        musicaPopular: "https://www.youtube.com/watch?v=gdZLi9oWNZg", // Dynamite
+        musicaPopular: "https://www.youtube.com/watch?v=gdZLi9oWNZg",
         wiki: "https://pt.wikipedia.org/wiki/BTS"
     },
     {
         titulo: "Dua Lipa",
-        descricao: "Cantora britânica de música pop conhecida por seus hits dançantes e estilo único.",
+        descricao: `Dua Lipa é uma artista britânica que se destacou no cenário pop com seus ritmos dançantes e estilo moderno. Seus hits contagiantes como "Levitating" a tornaram uma das vozes mais influentes da música pop contemporânea.`,
         redeSocial: "https://www.instagram.com/dualipa/",
-        musicaPopular: "https://www.youtube.com/watch?v=TUVcZfQe-Kw", // Levitating
+        musicaPopular: "https://www.youtube.com/watch?v=TUVcZfQe-Kw",
         wiki: "https://pt.wikipedia.org/wiki/Dua_Lipa"
     },
     {
         titulo: "Ed Sheeran",
-        descricao: "Cantor e compositor britânico conhecido por suas canções românticas e voz suave.",
+        descricao: `Ed Sheeran é um cantor e compositor britânico famoso por suas baladas emotivas e letras sinceras. Com sucessos como "Shape of You", ele se estabeleceu como um dos maiores nomes da música pop e folk moderna.`,
         redeSocial: "https://www.instagram.com/teddysphotos/",
-        musicaPopular: "hhttps://www.youtube.com/watch?v=JGwWNGJdvx8", // Shape of You
+        musicaPopular: "https://www.youtube.com/watch?v=JGwWNGJdvx8",
         wiki: "https://pt.wikipedia.org/wiki/Ed_Sheeran"
     },
     {
         titulo: "Lady Gaga",
-        descricao: "Cantora e compositora americana conhecida por sua excentricidade e versatilidade musical.",
+        descricao: `Lady Gaga é uma cantora e compositora americana conhecida por sua inovação musical e estilo extravagante. Desde "Just Dance" até "Shallow", ela se destacou por sua versatilidade e impacto cultural significativo.`,
         redeSocial: "https://www.instagram.com/ladygaga/",
-        musicaPopular: "https://www.youtube.com/watch?v=bo_efYhYU2A", // Shallow
+        musicaPopular: "https://www.youtube.com/watch?v=bo_efYhYU2A",
         wiki: "https://pt.wikipedia.org/wiki/Lady_Gaga"
     },
     {
         titulo: "Rihanna",
-        descricao: "Cantora barbadiana conhecida por seus hits dançantes e estilo fashion.",
+        descricao: `Rihanna é uma cantora barbadiana que se destacou com suas músicas dançantes e uma presença de palco marcante. Com hits como "Umbrella", ela se tornou uma das artistas mais influentes e inovadoras da indústria musical.`,
         redeSocial: "https://www.instagram.com/badgalriri/",
-        musicaPopular: "https://www.youtube.com/watch?v=CvBfHwUxHIk", // Umbrella
+        musicaPopular: "https://www.youtube.com/watch?v=CvBfHwUxHIk",
         wiki: "https://pt.wikipedia.org/wiki/Rihanna"
     },
     {
         titulo: "Justin Bieber",
-        descricao: "Cantor canadense que se tornou famoso ainda na adolescência.",
+        descricao: `Justin Bieber é um cantor canadense que ganhou fama global desde sua adolescência com uma série de sucessos pop. Com músicas como "Sorry", ele continuou a evoluir e a manter uma base de fãs fiel ao longo dos anos.`,
         redeSocial: "https://www.instagram.com/justinbieber/",
-        musicaPopular: "https://www.youtube.com/watch?v=fRh_vgS2dFE", // Sorry
+        musicaPopular: "https://www.youtube.com/watch?v=fRh_vgS2dFE",
         wiki: "https://pt.wikipedia.org/wiki/Justin_Bieber"
     }
 ]
-
--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log(dados)
+function pesquisar() {
+    let section = document.getElementById("resultados-pesquisa")
+
+    let resultados = ""
+
+    for (let dado of dados) {
+        resultados += `
+        <div class="item-resultado">
+            <h2>
+                <a href="${dado.redeSocial}" target="_blank">
+                    ${dado.titulo}
+                </a>
+            </h2>
+            
+            <p class="descricao-meta">
+                ${dado.descricao}
+            </p>
+    
+            <div class="item-buttons">
+                <a class="item-btn outline" href="${dado.wiki}" target="_blank">Mais informações</a>
+                <a class="item-btn" href="${dado.musicaPopular}" target="_blank">Música Popular</a>
+            </div>
+        </div>
+    `
+    }
+
+    section.innerHTML = resultados
+}
+
